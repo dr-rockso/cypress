678b9a1e4be8b696d725e0209bd66cde2f9545c0
AtofStryker <bglesias@gmail.com>
2024-10-28 15:57:29 -0400

    update vue3-vite-ts-configured to latest deps (need to fix typings issue within vue)

--- system-tests/projects/vue3-vite-ts-configured/src/components/HelloWorld.cy.ts
+++ system-tests/projects/vue3-vite-ts-configured/src/components/HelloWorld.cy.ts
@@ -2,6 +2,7 @@ import { mount } from 'cypress/vue'
 import HelloWorld from './HelloWorld.vue'
 
 it('works', () => {
+  // TODO: fix typings issue with mount
   mount(HelloWorld, {
     props: {
       msg: 'Hello!',
