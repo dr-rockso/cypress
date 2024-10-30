0a80f91ec8ca739b0ecbb565e820965ec7209d9b
AtofStryker <bglesias@gmail.com>
2024-10-30 19:27:42 -0400

    breaking: Cypress Component Testing no longer supports vue2 [run ci]

--- scripts/binary/trigger-publish-binary-pipeline.js
+++ scripts/binary/trigger-publish-binary-pipeline.js
@@ -10,7 +10,7 @@ const { getNextVersionForBinary } = require('../get-next-version')
   const { nextVersion } = await getNextVersionForBinary()
 
   const body = JSON.stringify({
-    branch: 'release/14.0.0',
+    branch: 'breaking/remove_vue2_support',
     parameters: {
       temp_dir: os.tmpdir(),
       sha: process.env.CIRCLE_SHA1,
