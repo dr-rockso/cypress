166624d0292708e9121929cf0416106c2859a8a2
Jennifer Shehane <jennifer@cypress.io>
2024-11-01 12:25:49 -0400

    make a test fail

--- packages/driver/cypress/e2e/commands/actions/click.cy.js
+++ packages/driver/cypress/e2e/commands/actions/click.cy.js
@@ -54,6 +54,10 @@ describe('src/cy/commands/actions/click', () => {
   })
 
   context('#click', () => {
+    it('fails', () => {
+      expect(true).to.be.false
+    })
+
     it('receives native click event', (done) => {
       const $btn = cy.$$('#button')
 
